8722eff86509e627ccc3792b79170d2207b5e092
罗琦 <luoqi04@meituan.com>
2018-09-08 22:57:51 +0800

    Sink类注释+BinaryOperator

---
+++ src/main/java/com/myjava/util/function/BinaryOperator.java
@@ -0,0 +1,19 @@
+package com.myjava.util.function;
+
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.function.BiFunction;
+
+@FunctionalInterface
+public interface BinaryOperator<T> extends BiFunction<T,T,T> {
+
+    public static <T> BinaryOperator<T> minBy(Comparator<? super T> comparator) {
+        Objects.requireNonNull(comparator);
+        return (a,b) -> comparator.compare(a,b) <= 0 ? a : b;
+    }
+
+    public static <T> BinaryOperator<T> maxBy(Comparator<? super T> comparator) {
+        Objects.requireNonNull(comparator);
+        return (a,b) -> comparator.compare(a,b) >= 0 ? a : b;
+    }
+}
--- src/main/java/com/myjava/util/stream/Sink.java
+++ src/main/java/com/myjava/util/stream/Sink.java
@@ -6,7 +6,21 @@ import java.util.Objects;
 
 /**
  * Sink是{@link Consumer}的扩展往往用于指导在流管道的每个步骤中的数值，较于传统管理大小信息的方法，
- * 控制流等。在第一次调用{@code accept}之前，你必须先调用{@code begin()}方法来通知数据的到来
+ * 控制流等。在第一次调用{@code accept}之前，你必须先调用{@code begin()}方法来通知数据的到来，待
+ * 所有的数据传送之后，你必须调用{@code end()}方法结束。在这之后你只有先调用{@code begin()}才能
+ * 再次调用{@code accept()}。{@code Sink}也提供了一种协作信号机制，用于在不打算接收数据时调用的
+ * {@code cancellationRequested()}，这样一个source就可以在把更多数据发送给{@code Sink}之前取出。
+ *
+ * 一个sink只有存在于两种状态中的一种：初始状态和激活状态。它开始于起始状态{@code begin()}方法可以将
+ * 其状态设置为激活，{@code end()}方法会将其转换为初始状态，这样达到了重用的目的。Data-accepting方法
+ * (比如{@code accept})只有在激活状态下才有效。
+ *
+ * 一个流管道包含了一个source，多个(也可能没有)中间步骤和最终的步骤像reduction或for-each。
+ *
+ * {@code Sink}实例用于表达管道的每个步骤，这个步骤可能接收到objects,ints,longs,doubles。Sink并不需要
+ * 一个特定的接口来对待基础类型。（它可能被叫做"kitchen sink"来表明兴趣趋势.）{@code Sink}实现了给定步骤
+ * 的组合被期望了解到下一个步骤的数据类型，在下游的{@code Sink}上调用正确的{@code accept()}方法。类似的，
+ * 每个步骤都必须实现正确的符合它可以接收的数据类型的{@code accept}方法。
  * @param <T> value流的元素类型
  */
 interface Sink<T> extends Consumer<T> {
