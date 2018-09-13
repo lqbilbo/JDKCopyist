697192ecb62fd7e69c0c800b8b9e73447e4de8cd
罗琦 <luoqi04@meituan.com>
2018-09-13 08:34:10 +0800

    ReadWriteLock注释1

--- src/main/java/com/myjava/util/concurrent/locks/ReadWriteLock.java
+++ src/main/java/com/myjava/util/concurrent/locks/ReadWriteLock.java
@@ -5,6 +5,7 @@ package com.myjava.util.concurrent.locks;
  * {@linkplain #readLock 读锁}被多个读线程同时持有，然而不能有写操作。
  * {@linkplain #writeLock 写锁}却是独占的。
  *
+ * 所有的{@code ReadWriteLock}实现必须保证{@code writeLock}操作的内存同步也要持有
  */
 public interface ReadWriteLock {
 
